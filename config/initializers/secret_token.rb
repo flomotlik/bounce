32aa552e2667fd3c841d60f2be1c492f61119c73
Florian Motlik <flomotlik@gmail.com>
2012-03-27 17:37:07 +0200

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bounce::Application.config.secret_token = 'ec012044c38b820f4952003be11d08f883b021d069ff1ca7fbc30c0ea3401d998cde5bdee44c99413f3f951ba679bd4c3f4d390b4ce5a2ee427a5ad2bf31bb94'
